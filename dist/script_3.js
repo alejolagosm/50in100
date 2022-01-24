064f8af234692302737dd603adccb74fbca1cbe4
alejolagosm <falame94@hotmail.com>
2022-01-24 15:03:19 -0500

    Added project 22: Fun with buttons

--- dist/script_3.js
+++ dist/script_3.js
@@ -15,4 +15,33 @@ burger.addEventListener("click", () => {
 // ////////////////////////////////////////////////////////////////////////////
 // Project 22: Fun with buttons
 if (document.querySelector(".project-22") != null) {
+  const btn_1 = document.querySelector(".btn_1");
+
+  btn_1.addEventListener("click", (e) => {
+    document.querySelector(".circle").classList.remove("animate");
+    const x = e.clientX;
+    const y = e.clientY;
+    const btntop = e.target.closest(".btn_1").getBoundingClientRect().top;
+    const btnleft = e.target.closest(".btn_1").getBoundingClientRect().left;
+    const btnwidth = e.target.closest(".btn_1").getBoundingClientRect().width;
+    const btnheight = e.target.closest(".btn_1").getBoundingClientRect().height;
+    console.log(btnwidth, btnheight);
+    btn_1.style.setProperty("--top", `${(100 * (y - btntop)) / btnheight}%`);
+    btn_1.style.setProperty("--left", `${(100 * (x - btnleft)) / btnwidth}%`);
+    document.querySelector(".circle").classList.add("animate");
+  });
+
+  const btn_6 = document.querySelector(".btn_6");
+  const circle_6 = document.getElementById("circle_6");
+  const text_6 = document.querySelector(".text_6");
+
+  btn_6.addEventListener(
+    "click",
+    (e) => {
+      btn_6.classList.toggle("active");
+      text_6.classList.toggle("hidden");
+      circle_6.classList.toggle("active");
+    },
+    { once: true }
+  );
 }
